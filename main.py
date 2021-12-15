62e61ab8ff027b049f0cc474c5e38841ba1e0f32
Can Berk Durmus <canberkdurmus@gmail.com>
2021-12-15 13:46:29 +0100

    Update parameters on time-lapse object instantiation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from time_lapse import TimeLapse
 
 if __name__ == '__main__':
-    tl = TimeLapse(1, max_frames=3600)
+    tl = TimeLapse(1, max_frames=240, resolution=(1280, 720), fps=24, data_folder='image')
     tl.thread.start()
     tl.thread.join()
